373de7f3acaee848e32f39049037f812822266bc
O-Seonsik <dhtjstlr777@gmail.com>
2020-04-01 00:51:59 +0900

    Learning for Handling User Input in SwiftUI

--- SwiftUI_test/LandmarkList.swift
+++ SwiftUI_test/LandmarkList.swift
@@ -9,14 +9,24 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @EnvironmentObject var userData: UserData
+
     var body: some View {
-        NavigationView{
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)){
-                    LandmarkRow(landmark: landmark)
+        NavigationView {
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
-        .navigationBarTitle(Text("Landmarks"))
+            .navigationBarTitle(Text("Landmarks"))
         }
     }
 }
@@ -24,5 +34,6 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environmentObject(UserData())
     }
 }
